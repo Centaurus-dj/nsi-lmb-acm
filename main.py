5bc599a3745f9972e6942fb1a6897328f12e3918
Centaurus <alexis.opolka@protonmail.com>
2022-04-01 08:23:57 +0200

    Add 2 screen separation + other modifications
    I added a separation of game screen and start screen with a draft of Rect Buttons + adding preventive code of executing the main.py file as an import with the condition

--- main.py
+++ main.py
@@ -2,43 +2,89 @@
 Main file, execute initialization of process, variables, etc...
 
 """
-import projectile
-from game import *
-from projectile import *
+import pygame
+from game import Game
 
-pygame.init()
-projectile_ = projectile.Projectile
-pygame.display.set_caption("jeux")
-screen = pygame.display.set_mode((1080, 600))
-background = pygame.image.load('assets/bg.jpg')
-background = pygame.transform.scale(background, (1080, 600))
-game = Game()
 
-running = True
+def show_startscreen():
+    global size
 
-while running:
+    pygame.display.set_caption("Alone on the start screen - Alexis O., Chloé Sch., Matéo D.")
 
-    screen.blit(background, (0, 0))
+    # The background color can be whatever you want
+    screen_background = pygame.transform.scale(pygame.image.load("assets/main_background.jpg"), size)
+    start_btn = pygame.transform.scale(pygame.image.load("assets/Boutons/start.png"), (41*5, 15*5))
+    saves_btn = pygame.transform.scale(pygame.image.load("assets/Boutons/settings.png"), (16*3, 16*3))
 
-    game.update(screen)
+    screen = pygame.display.set_mode(size)
 
+    ### Show Elements on screen
+    screen.blit(screen_background, (0, 0))
+    start_b = screen.blit(start_btn, (440, 10))
+    screen.blit(saves_btn, (650, 34))
     pygame.display.flip()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
 
-        elif event.type == pygame.KEYDOWN:
-            game.pressed[event.key] = True
-            if event.key == pygame.K_z:
-                projectile_.shoot_up()
-            if event.key == pygame.K_q:
-                game.player.launch_projectile()
-            if event.key == pygame.K_s:
-                game.player.launch_projectile()
-            if event.key == pygame.K_d:
-                game.player.launch_projectile()
+    ### While loop to stay displayed until input of user
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                quit()
 
-        elif event.type == pygame.KEYUP:
-            game.pressed[event.key] = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                ## if mouse is pressed get position of cursor ##
+                pos = pygame.mouse.get_pos()
+                ## check if cursor is on button ##
+                if start_b.collidepoint(pos):
+                    running = False
+                    pygame.quit()
+
+def main():
+    pygame.display.set_caption("Alone in a room - Alexis O., Chloé Sch., Matéo D.")
+    screen = pygame.display.set_mode(size)
+    background = pygame.image.load('assets/background.jpg')
+    background = pygame.transform.scale(background, size)
+
+    game = Game()
+
+    running = True
+
+    while running:
+
+        screen.blit(background, (0, 0))
+
+        game.update(screen)
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+
+            elif event.type == pygame.KEYDOWN:
+                game.pressed[event.key] = True
+                if event.key == pygame.K_z:
+                    game.fireball.shoot_up()
+                if event.key == pygame.K_q:
+                    game.player.launch_projectile()
+                if event.key == pygame.K_s:
+                    game.player.launch_projectile()
+                if event.key == pygame.K_d:
+                    game.player.launch_projectile()
+
+            elif event.type == pygame.KEYUP:
+                game.pressed[event.key] = False
+
+if __name__ == "__main__":
+
+    (width, height) = (1080, 720)
+    size = (width, height)
+
+    pygame.init()
+
+    show_startscreen()
+    main()
